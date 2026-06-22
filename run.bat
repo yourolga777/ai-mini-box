2f04282213117b74b2561908b71c1a1fbef18c90
Olga <yourolga777@gmail.com>
2026-06-22 21:54:45 +0300

    fix: check data\config.json instead of config.json in run.bat

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ cd /d "%~dp0"
 echo Installing packages...
 pip install -e packages/core/ -q
 pip install -e packages/web/ -q
-if not exist "config.json" python -m ai_mini_box init
+if not exist "data\config.json" python -m ai_mini_box init
 start "" http://127.0.0.1:8080
 echo Starting server at http://127.0.0.1:8080
 python -m ai_mini_box serve
